7a759cea12d09471ecb80692bab99dfa325a9a4f
Crowdbotics <team@crowdbotics.com>
2021-10-13 07:56:03 +0000

    Initial commit.

---
+++ migoze_31373/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for migoze_31373 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'migoze_31373.settings')
+
+application = get_wsgi_application()
